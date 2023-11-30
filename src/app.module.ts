5ef5489b9c748d9b197ce7bc9d8c4ebc89d9f016
montejs3 <98852422+montejs3@users.noreply.github.com>
2023-11-30 08:26:28 -0500

    ListoParcial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'admin',
+    database: 'musica',
+    entities: [ ],
+    synchronize: true,
+    dropSchema: true,
+    keepConnectionAlive: true
+  }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
